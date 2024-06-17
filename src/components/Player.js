356c852f36d85c7c2a9f67fa5eab5dbbd165a396
so23go <so23go@naver.com>
2024-06-17 17:24:02 +0900

    [Feat] 테이블에 맞게 위치 세팅

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../styles/Player.css';
 
-const Player = ({ name, tableNum, isWinner }) => {
+const Player = ({ name, tableNum, isWinner, position }) => {
+
+  const playerImage = require(`../assets/${name}.png`);
+
   return (
-    <div className="player">
-      <span>{name} - 자리{tableNum}</span>
-      {isWinner === "Y" && <span className="crown">👑</span>}
+    <div className="player" style={position}>
+      <div className='player-container'>
+        {isWinner === "Y" && <span className="crown">👑</span>}
+        <img src={playerImage} alt={name} className="player-image" />
+        <span className='player-name'>{name} - 자리 {tableNum}</span>
+      </div>
+
     </div>
   );
 };
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Player from './Player';
 import tableImg from '../assets/Table.png';
 import '../styles/Table.css';
@@ -6,31 +6,42 @@ import mockPlayerData from '../data/mockPlayers.json'
 
 const Table = () => {
 
-    const [players, setPlayer] = useState([]);
+  const [players, setPlayer] = useState([]);
 
-    useEffect(() => {
-        if (mockPlayerData && mockPlayerData.data && Array.isArray(mockPlayerData.data.playerList)) {
-            setPlayer(mockPlayerData.data.playerList);
-        } else {
-            console.error('Data format is incorrect');
-        }
-    }, []);
-  
-    return (
-      <div className="table">
-        <img className="tableImage" src={tableImg}></img>
+  useEffect(() => {
+    if (mockPlayerData && mockPlayerData.data && Array.isArray(mockPlayerData.data.playerList)) {
+      setPlayer(mockPlayerData.data.playerList);
+    } else {
+      console.error('Data format is incorrect');
+    }
+  }, []);
 
-        {players.map((player, index) => (
-          <Player
-            key={index}
-            tableNum = {player.tableNum}
-            name={player.name}
-            isWinner={player.isWinner}
-            
-          />
-        ))}
-      </div>
-    );
+  const tablePositions = {
+    0: { top: '55%', left: '15%' },
+    1: { top: '58%', left: '37%' },
+    2: { top: '58%', left: '59%' },
+    3: { top: '55%', left: '83%' },
+    4: { top: '30%', left: '83%' },
+    5: { top: '22%', left: '59%' },
+    6: { top: '22%', left: '37%' },
+    7: { top: '30%', left: '15%' }
   };
-  
-  export default Table;
+
+  return (
+    <div className="table-container">
+      <img className="tableImage" src={tableImg} alt="Table" />
+
+      {players.map((player, index) => (
+        <Player
+          key={index}
+          tableNum={player.tableNum}
+          name={player.name}
+          isWinner={player.isWinner}
+          position={tablePositions[player.tableNum]} // 테이블 번호에 따른 위치를 전달합니다.
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Table;
